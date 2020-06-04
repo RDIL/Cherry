57e4e7124049ad5652f5799a62a661d785570bcf
Reece Dunham <me@rdil.rocks>
2020-06-04 01:48:38 +0000

    Fix the crash
    Signed-off-by: Reece Dunham <me@rdil.rocks>

--- src/main/java/rocks/rdil/cherry/mixins/MixinConnectScreen.java
+++ src/main/java/rocks/rdil/cherry/mixins/MixinConnectScreen.java
@@ -2,14 +2,11 @@ package rocks.rdil.cherry.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 
 import net.minecraft.client.gui.screen.ConnectScreen;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.util.NarratorManager;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Util;
 
 @Mixin(ConnectScreen.class)
 public abstract class MixinConnectScreen extends Screen {
@@ -18,8 +15,7 @@ public abstract class MixinConnectScreen extends Screen {
     }
 
     private int spinnerFrame = 0;
-    @Shadow Text status = null;
-    @Shadow long field_19097 = (long) 0F;
+    public Text status = new TranslatableText("connect.connecting", new Object[0]);
 
     /**
      * Render the screen.
@@ -30,13 +26,8 @@ public abstract class MixinConnectScreen extends Screen {
     @Overwrite
     public void render(int mouseX, int mouseY, float delta) {
         this.renderBackground();
-        long l = Util.getMeasuringTimeMs();
-        if (l - this.field_19097 > 2000L) {
-            this.field_19097 = l;
-            NarratorManager.INSTANCE.narrate((new TranslatableText("narrator.joining", new Object[0])).getString());
-        }
 
-        String[] frames = new String[]{"|", "/", "-", "\\"};
+        final String[] frames = new String[]{"|", "/", "-", "\\"};
         this.spinnerFrame = (this.spinnerFrame == 3? 0 : this.spinnerFrame + 1);
 
         this.drawCenteredString(this.font, this.status.asFormattedString(), this.width / 2, this.height / 2 - 50, 16777215);
