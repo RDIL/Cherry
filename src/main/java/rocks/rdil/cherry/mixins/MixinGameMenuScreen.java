7a26cb5d114167f8af1d46647e3028d12de69e6e
Reece Dunham <me@rdil.rocks>
2020-06-04 16:02:00 +0000

    Config: Add button to pause menu

---
+++ src/main/java/rocks/rdil/cherry/mixins/MixinGameMenuScreen.java
@@ -0,0 +1,26 @@
+package rocks.rdil.cherry.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import rocks.rdil.cherry.gui.CherryGui;
+
+@Mixin(GameMenuScreen.class)
+public class MixinGameMenuScreen extends Screen {
+    protected MixinGameMenuScreen(Text title) {
+        super(title);
+    }
+
+    @Inject(at = @At(value = "RETURN"), method = "initWidgets")
+    public void initWidgetsTail(CallbackInfo ci) {
+        this.addButton(new ButtonWidget(10, 10, 100, 20, "Cherry Settings", buttonWidget -> {
+            this.minecraft.openScreen(new CherryGui());
+        }));
+    }
+}
