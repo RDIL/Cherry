fe68489b514b096beba9f6644e7ebf8600d9ba0a
Reece Dunham <me@rdil.rocks>
2020-06-07 20:40:54 +0000

    Translate some kotlin to java, also more work
    Signed-off-by: Reece Dunham <me@rdil.rocks>

--- src/kotlin/rocks/rdil/simpleconfig/Annotations.kt
+++
@@ -1,8 +0,0 @@
-package rocks.rdil.simpleconfig
-
-@DslMarker
-annotation class AnnotationDsl
-
-@AnnotationDsl
-@Target(AnnotationTarget.FIELD)
-annotation class Configuration(val alt: String = "")
---
+++ src/main/java/rocks/rdil/cherry/config/CherryOptions.java
@@ -0,0 +1,8 @@
+package rocks.rdil.cherry.config;
+
+import rocks.rdil.simpleconfig.Configuration;
+
+public class CherryOptions {
+    @Configuration private boolean toggleSprint;
+    @Configuration private boolean enableTutorialPopups;
+}
