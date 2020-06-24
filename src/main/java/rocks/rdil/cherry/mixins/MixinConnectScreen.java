5f342f7948653d0613735c1a98e916336b9d6414
Reece Dunham <me@rdil.rocks>
2020-06-24 21:01:18 +0000

    Avoid doing a full overwrite

--- src/main/java/rocks/rdil/cherry/mixins/MixinConnectScreen.java
+++ src/main/java/rocks/rdil/cherry/mixins/MixinConnectScreen.java
@@ -1,12 +1,13 @@
 package rocks.rdil.cherry.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 
 import net.minecraft.client.gui.screen.ConnectScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ConnectScreen.class)
 public abstract class MixinConnectScreen extends Screen {
@@ -16,24 +17,12 @@ public abstract class MixinConnectScreen extends Screen {
     }
 
     private int spinnerFrame = 0;
-    @Shadow private Text status;
 
-    /**
-     * Render the screen.
-     * 
-     * @author Reece Dunham and Mojang
-     * @reason Add the spinner while connecting.
-     */
-    @Overwrite
-    public void render(int mouseX, int mouseY, float delta) {
-        this.renderBackground();
-
+    @Inject(at = @At(value = "RETURN"), method = "render")
+    public void render(int mouseX, int mouseY, float delta, CallbackInfo ci) {
         final String[] frames = new String[]{"|", "/", "-", "\\"};
         this.spinnerFrame = (this.spinnerFrame == 3? 0 : this.spinnerFrame + 1);
 
-        this.drawCenteredString(this.font, this.status.asFormattedString(), this.width / 2, this.height / 2 - 50, 16777215);
         this.drawCenteredString(this.font, frames[this.spinnerFrame], this.width / 2, this.height / 2, 16777215);
-
-        super.render(mouseX, mouseY, delta);
     }
 }
