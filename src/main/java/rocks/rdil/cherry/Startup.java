5c5cf6e822b33d7948d0af345082112247501040
Reece <me@rdil.rocks>
2020-06-24 12:19:05 -0400

    Update checking!
    Signed-off-by: Reece <me@rdil.rocks>

--- src/main/java/rocks/rdil/cherry/Startup.java
+++ src/main/java/rocks/rdil/cherry/Startup.java
@@ -4,7 +4,10 @@ import java.io.File;
 
 import lombok.NoArgsConstructor;
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import rocks.rdil.cherry.config.CherryOptions;
+import rocks.rdil.cherry.utils.UpdateChecker;
 import rocks.rdil.simpleconfig.ConfigHandler;
 
 /**
@@ -13,8 +16,12 @@ import rocks.rdil.simpleconfig.ConfigHandler;
 @NoArgsConstructor
 public final class Startup implements ModInitializer {
     public final ConfigHandler configHandler = new ConfigHandler(new File("cherry-config.json"));
+    public static final String VERSION = "1.0.0";
     public static Startup instance;
     private static boolean initialized;
+    private static final UpdateChecker uc = new UpdateChecker();
+    public static final Logger LOGGER = LogManager.getLogger("Cherry");
+    public boolean hasUpdate = false;
 
     @Override
     public void onInitialize() {
@@ -25,7 +32,17 @@ public final class Startup implements ModInitializer {
         initialized = true;
         instance = this;
 
+        LOGGER.info("Loaded Cherry!");
+
         configHandler.register(CherryOptions.INSTANCE);
+
+        LOGGER.info("Checking for updates...");
+        this.hasUpdate = !uc.isUpToDate();
+        if (this.hasUpdate) {
+            LOGGER.warn("An update is available!");
+        } else {
+            LOGGER.info("No updates detected.");
+        }
     }
 
     public void saveConfig() {
