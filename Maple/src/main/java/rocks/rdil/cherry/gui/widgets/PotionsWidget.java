cabc4828ae1021e17c64c1c92d2f8bf6c15ebd6b
Reece Dunham <me@rdil.rocks>
2020-07-12 01:53:16 +0000

    Add Cherry Maple (the infinite snapshot)
    Signed-off-by: Reece Dunham <me@rdil.rocks>

---
+++ Maple/src/main/java/rocks/rdil/cherry/gui/widgets/PotionsWidget.java
@@ -0,0 +1,54 @@
+package rocks.rdil.cherry.gui.widgets;
+
+import com.google.common.collect.Ordering;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffectUtil;
+import rocks.rdil.cherry.config.CherryOptions;
+
+import java.util.Collection;
+
+public class PotionsWidget extends Widget {
+    @Override
+    public String getPrefix() {
+        return null;
+    }
+
+    @Override
+    public String getSuffix() {
+        return null;
+    }
+
+    @Override
+    public int getConfigX() {
+        return CherryOptions.INSTANCE.potWidgetX;
+    }
+
+    @Override
+    public int getConfigY() {
+        return CherryOptions.INSTANCE.potWidgetY;
+    }
+
+    @Override
+    public boolean getEnabled() {
+        return CherryOptions.INSTANCE.enablePotionsWidget;
+    }
+
+    @Override
+    public void render(DrawableHelper s) {
+        if (MinecraftClient.isHudEnabled() && this.getEnabled()) {
+            int y = 0;
+            Collection<StatusEffectInstance> collection = MinecraftClient.getInstance().player.getStatusEffects();
+            if (!collection.isEmpty()) {
+                for (StatusEffectInstance statusEffectInstance : Ordering.natural().reverse().sortedCopy(collection)) {
+                    StatusEffect statusEffect = statusEffectInstance.getEffectType();
+                    s.drawCenteredString(MinecraftClient.getInstance().textRenderer, I18n.translate(statusEffect.getTranslationKey()) + " - " + StatusEffectUtil.durationToString(statusEffectInstance, 1.0F), this.getConfigX(), this.getConfigY() + y, 16777215);
+                    y += 10;
+                }
+            }
+        }
+    }
+}
---
+++ Maple/src/main/java/rocks/rdil/cherry/mixins/MixinConnectScreen.java
@@ -0,0 +1,27 @@
+package rocks.rdil.cherry.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ConnectScreen.class)
+public abstract class MixinConnectScreen extends Screen {
+    protected MixinConnectScreen(Text title) {
+        super(title);
+    }
+
+    private int spinnerFrame = 0;
+
+    @Inject(at = @At(value = "RETURN"), method = "render")
+    public void render(int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        final String[] frames = new String[]{"|", "/", "-", "\\"};
+        this.spinnerFrame = (this.spinnerFrame == 3? 0 : this.spinnerFrame + 1);
+
+        this.drawCenteredString(this.textRenderer, frames[this.spinnerFrame], this.width / 2, this.height / 2, 16777215);
+    }
+}
